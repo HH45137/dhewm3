70d9eb041efa9fd877bf54e1d85978aa944120fc
RISC-1145 <wtpp647@gmail.com>
2026-04-25 14:19:28 +0800

    rhi: Add CreateBuffer and CreateTexture

--- neo/renderer/rhi/idRHI.h
+++ neo/renderer/rhi/idRHI.h
@@ -1,18 +1,9 @@
 #pragma once
 
+#include <cstdint>
 #include <string>
 #include <vector>
 
-
-enum RHIDataFormat {
-    RHI_DATA_FORMAT_R_8,
-    RHI_DATA_FORMAT_RG_8,
-    RHI_DATA_FORMAT_RGB_8,
-    RHI_DATA_FORMAT_RGBA_8,
-    RHI_DATA_FORMAT_UNKNOWN,
-
-    RHI_DATA_FORMAT_COUNT
-};
 
 enum RHIFeature {
     RHI_FEATURE_RAY_TRACING,
@@ -22,20 +13,85 @@ enum RHIFeature {
     RHI_FEATURE_COUNT
 };
 
+enum RHIResourceState {
+    RHI_RESOURCE_STATE_UNINITIALIZED,
+    RHI_RESOURCE_STATE_INITIALIZED,
+
+    RHI_RESOURCE_STATE_COUNT
+};
+
+enum RHIBufferUsage {
+    RHI_BUFFER_USAGE_VERTEX,
+    RHI_BUFFER_USAGE_FRAGMENT,
+    RHI_BUFFER_USAGE_UNIFORM,
+    RHI_BUFFER_USAGE_STORAGE,
+    RHI_BUFFER_USAGE_STAGING,
+
+    RHI_BUFFER_USAGE_COUNT
+};
+
+enum RHIMemoryType {
+    RHI_MEMORY_TYPE_HOST,
+    RHI_MEMORY_TYPE_DEVICE,
+    RHI_MEMORY_TYPE_SHARED,
+
+    RHI_MEMORY_TYPE_COUNT
+};
+
+enum RHITextureSamplerType {
+    RHI_SAMPLER_TYPE_NEAREST,
+    RHI_SAMPLER_TYPE_LINEAR,
+
+    RHI_SAMPLER_TYPE_COUNT
+};
+
+enum RHITextureAddressType {
+    RHI_TEXTURE_ADDRESS_TYPE_REPEAT,
+    RHI_TEXTURE_ADDRESS_TYPE_MIRROR_REPEAT,
+    RHI_TEXTURE_ADDRESS_TYPE_CLAMP_TO_EDGE,
+    RHI_TEXTURE_ADDRESS_TYPE_CLAMP_TO_BORDER,
+
+    RHI_TEXTURE_ADDRESS_TYPE_COUNT
+};
+
+enum RHITextureFormat {
+    RHI_DATA_TEXTURE_R_8,
+    RHI_DATA_TEXTURE_RG_8,
+    RHI_DATA_TEXTURE_RGB_8,
+    RHI_DATA_TEXTURE_RGBA_8,
+    RHI_DATA_TEXTURE_UNKNOWN,
+
+    RHI_DATA_TEXTURE_COUNT
+};
+
 struct RHIAdapterInfo {
     std::string name;
     size_t ram_size;
     size_t max_texture_size;
     size_t max_uniform_buffer_range;
-    std::vector<RHIDataFormat> formats;
+    std::vector<RHITextureFormat> formats;
     std::vector<RHIFeature> features;
 };
 
 struct RHIResource {
+    RHIResourceState state;
     size_t ref_count;
     bool is_destroy;
+    std::string debug_name;
 };
 
-struct RHIBuffer : RHIResource{
+struct RHIBuffer : RHIResource {
+    RHIBufferUsage usage;
+    RHIMemoryType memory_type;
+    uint32_t size;
+    uint32_t stride;
+};
 
+struct RHITexture : RHIResource {
+    RHITextureFormat usage;
+    RHITextureSamplerType sampler_type;
+    RHITextureAddressType address_type;
+    uint32_t width, height, depth;
+    uint32_t mip_levels;
+    uint32_t sample_count;
 };
--- neo/renderer/rhi/idRHIDevice.h
+++ neo/renderer/rhi/idRHIDevice.h
@@ -5,8 +5,27 @@
 
 class RHIDevice {
 public:
-    static RHIDevice* Create(void* window_handle);
     virtual ~RHIDevice() = default;
 
-    virtual const RHIAdapterInfo* GetAdapterInfo() = 0;
+    virtual const RHIDevice *Create(void *window_handle);
+
+    virtual const RHIAdapterInfo *GetAdapterInfo() = 0;
+
+    virtual const RHIBuffer *CreateBuffer(
+        RHIBufferUsage usage,
+        RHIMemoryType memory_type,
+        uint32_t size,
+        uint32_t stride
+    ) = 0;
+
+    virtual const RHITexture *CreateTexture(
+        RHITextureFormat usage,
+        RHITextureSamplerType sampler_type,
+        RHITextureAddressType address_type,
+        uint32_t width,
+        uint32_t height,
+        uint32_t depth,
+        uint32_t mip_levels,
+        uint32_t sample_count
+    ) = 0;
 };
